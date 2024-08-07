7f914613b0617819a23b9683b4a213f33cdcf8ca
h-mahmoodi <hesamahmoodi@gmail.com>
2024-08-07 18:18:45 +0200

    update webpack dev and prod mode

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,66 +4,69 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.tsx'),
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.[jt]sx?$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-      },
-      {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline'
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html')
-    }),
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin({
-      patterns: [
+module.exports = (env: { env: string }) => {
+  const isProduction = env.env === 'production';
+  return {
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js'
+    },
+    mode: isProduction ? 'production' : 'development',
+    module: {
+      rules: [
         {
-          from: 'public',
-          to: '.',
-          globOptions: {
-            ignore: ['**/index.html']
-          }
+          test: /\.[jt]sx?$/,
+          use: ['babel-loader'],
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+        },
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+        },
+        {
+          test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+          type: 'asset/resource'
+        },
+        {
+          test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+          type: 'asset/inline'
         }
       ]
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles.css'
-    })
-  ],
-  devServer: {
-    static: path.join(__dirname, 'src'),
-    port: 3001,
-    hot: 'only',
-    compress: true,
-    open: true
-  }
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.jsx']
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html')
+      }),
+      new CleanWebpackPlugin(),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'public',
+            to: '.',
+            globOptions: {
+              ignore: ['**/index.html']
+            }
+          }
+        ]
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'styles.css'
+      })
+    ],
+    devServer: {
+      static: path.join(__dirname, 'src'),
+      port: 3001,
+      hot: 'only',
+      compress: true,
+      open: true
+    }
+  };
 };
